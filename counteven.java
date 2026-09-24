4665dc58bf5f50d78f0f7eb5673f1c9b8806491a
Deepa1626 <deepanjalis16072006@gmail.com>
2026-09-24 09:55:33 +0530

    count even number

---
+++ counteven.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class counteven {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int [] arr = new int[n];
+        
+
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        int count=0;
+        for(int e:arr){
+            if(e % 2==0){
+                count++;
+
+            }
+        }
+        System.out.println("number of even element"+count);
+    }
+    
+}
